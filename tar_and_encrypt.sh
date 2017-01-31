0c2efb2a9fa82d738acc81adb942341e3f151bdd
Maria Pacana <maria.pacana@gmail.com>
2017-01-31 09:04:33 -0800

    Automatically make new dir based on source dir name and pass in source dir and pub key path as args

--- tar_and_encrypt.sh
+++ tar_and_encrypt.sh
@@ -5,11 +5,14 @@
 # Encrypt symmetric key with public key and encrypt tarball with symmetric key.
 # Store tarball and encrypted symmetric key in backup/
 compress_and_encrypt() {
-	mkdir backup
-	openssl rand -base64 32 > backup/key.bin
-	openssl rsautl -encrypt -inkey /home/maria/public.pem -pubin -in backup/key.bin -out backup/key.bin.enc
-  tar -zc "$@" | openssl enc -aes-256-cbc -salt -in /dev/stdin -out backup/backup.enc -pass file:./backup/key.bin
-	rm backup/key.bin	
+	BACKUP_SOURCE_DIR=$1
+  BACKUP_DEST_DIR=$2
+	PUBLICKEY=$3
+  mkdir $BACKUP_DEST_DIR
+	openssl rand -base64 32 > "$BACKUP_DEST_DIR"/key.bin
+	openssl rsautl -encrypt -inkey $PUBLICKEY -pubin -in "$BACKUP_DEST_DIR"/key.bin -out "$BACKUP_DEST_DIR"/key.bin.enc
+  tar -zc "$BACKUP_SOURCE_DIR" | openssl enc -aes-256-cbc -salt -in /dev/stdin -out "$BACKUP_DEST_DIR"/backup.enc -pass file:"$BACKUP_DEST_DIR"/key.bin
+	rm "$BACKUP_DEST_DIR"/key.bin	
 }
 
 # Decrypt symmetric key with private key. 
--- upload_download.sh
+++ upload_download.sh
@@ -3,9 +3,19 @@
 source ./amazon/tar_and_encrypt.sh
 
 upload() {
-	compress_and_encrypt "$@"
-	s3cmd sync backup s3://sibilance-backup
-	rm -rf backup
+	BACKUP_SOURCE_DIR="$1"
+	BACKUP_DEST_DIR="$1"-backup
+	PUBLICKEY=$2
+	AMAZON_S3_DIR=$3
+
+	if [ $# -lt 3 ]; then
+  	echo 1>&2 "$0: Not enough arguments. 'upload [source-dir] [/path/to/publickey] [s3-folder]'"
+  	exit 1
+  fi
+
+  compress_and_encrypt $BACKUP_SOURCE_DIR $BACKUP_DEST_DIR $PUBLICKEY
+	s3cmd sync $BACKUP_DEST_DIR $AMAZON_S3_DIR
+	rm -rf $BACKUP_DEST_DIR
 }
 
 
