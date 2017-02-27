18661b5a9bb9c5115e1aed80a0be20f04626f794
Maria Pacana <maria.pacana@gmail.com>
2017-02-26 16:00:37 -0800

    Anonymize further

--- tar_and_encrypt.sh
+++ tar_and_encrypt.sh
@@ -5,27 +5,32 @@
 # Encrypt symmetric key with public key and encrypt tarball with symmetric key.
 # Store tarball and encrypted symmetric key in backup/
 compress_and_encrypt() {
-	BACKUP_SOURCE_DIR=$1
+  BACKUP_SOURCE_DIR=$1
   BACKUP_DEST_DIR=$2
-	PUBLICKEY=$3
+  PUBLICKEY=$3
   mkdir $BACKUP_DEST_DIR
-	openssl rand -base64 32 > "$BACKUP_DEST_DIR"/key.bin
-	openssl rsautl -encrypt -inkey $PUBLICKEY -pubin -in "$BACKUP_DEST_DIR"/key.bin -out "$BACKUP_DEST_DIR"/key.bin.enc
+  openssl rand -base64 32 > "$BACKUP_DEST_DIR"/key.bin
+  openssl rsautl -encrypt -inkey $PUBLICKEY -pubin -in "$BACKUP_DEST_DIR"/key.bin -out "$BACKUP_DEST_DIR"/key.bin.enc
   tar -zc "$BACKUP_SOURCE_DIR" | openssl enc -aes-256-cbc -salt -in /dev/stdin -out "$BACKUP_DEST_DIR"/backup.enc -pass file:"$BACKUP_DEST_DIR"/key.bin
-	rm "$BACKUP_DEST_DIR"/key.bin	
+  rm "$BACKUP_DEST_DIR"/key.bin	
 }
 
 # Decrypt symmetric key with private key. 
 # Decrypt tarball with decrypted symmetric key.
 # Uncompress tarball into recovered_backup/
 decrypt_and_uncompress() {
-	if [ $# -lt 3 ]; then
-  	echo 1>&2 "$0: Not enough arguments. Need private key, symmetric key, backup file"
-  exit 1
+  PRIVATEKEY=$1
+  BACKUP_DIR=$2
+
+  if [ $# -lt 2 ]; then
+    echo 1>&2 "$0: Not enough arguments. 'decrypt_and_uncompress [path/to/privatekey] [path/to/backupdir]'"
+    exit 1
   fi
-	openssl rsautl -decrypt -inkey $1 -in $2 -out key.bin 
-	mkdir amazon_backup_decrypted
-	openssl enc -d -aes-256-cbc -in $3 -out /dev/stdout -pass file:./key.bin | tar -zxvf /dev/stdin -C amazon_backup_decrypted
-	rm key.bin
+ 
+  echo "Private Key: $PRIVATEKEY"
+  echo "$BACKUP_DIR"/key.bin.enc
+  openssl rsautl -decrypt -inkey $PRIVATEKEY -in "$BACKUP_DIR"/key.bin.enc -out "$BACKUP_DIR"/key.bin 
+  openssl enc -d -aes-256-cbc -in "$BACKUP_DIR"/backup.enc -out /dev/stdout -pass file:"$BACKUP_DIR"/key.bin | tar -zxvf /dev/stdin -C $BACKUP_DIR
+  rm "$BACKUP_DIR"/key.bin "$BACKUP_DIR"/key.bin.enc "$BACKUP_DIR"/backup.enc
 }
 
--- upload_download.sh
+++ upload_download.sh
@@ -4,7 +4,7 @@ source ./amazon/tar_and_encrypt.sh
 
 upload() {
   BACKUP_SOURCE_DIR="$1"
-  BACKUP_DEST_DIR="$1"-backup
+  BACKUP_DEST_DIR=~/tmp/"$(basename $BACKUP_SOURCE_DIR)"-backup
   PUBLICKEY=$2
   AMAZON_S3_DIR=$3
 
@@ -12,6 +12,11 @@ upload() {
     echo 1>&2 "$0: Not enough arguments. 'upload [source-dir] [/path/to/publickey] [s3-folder]'"
     exit 1
   fi
+
+	echo "Backup Source Directory: $BACKUP_SOURCE_DIR"
+  echo "Backup Destination Dir: $BACKUP_DEST_DIR"
+  echo "PUBLICKEY: $PUBLICKEY"
+  echo "Amazon S3 Dir: $AMAZON_S3_DIR"
 
   compress_and_encrypt $BACKUP_SOURCE_DIR $BACKUP_DEST_DIR $PUBLICKEY
   s3cmd sync $BACKUP_DEST_DIR $AMAZON_S3_DIR
@@ -29,7 +34,13 @@ download() {
     exit 1
   fi
 
+  echo "Amazon Source Directory: $AMAZON_SOURCE_DIR"
+  echo "Private Key Path: $PRIVATE_KEY"
+  echo "Backup Destination: $BACKUP_DEST_DIR"
+
+  mkdir $BACKUP_DEST_DIR
+
   s3cmd get "$AMAZON_SOURCE_DIR"/backup.enc $BACKUP_DEST_DIR
-  s3cmd get "$AMAZON_DEST_DIR"/key.bin.enc $BACKUP_DEST_DIR
-  decrypt_and_uncompress $PRIVATEKEY "$BACKUP_DEST_DIR"/key.bin.enc "$BACKUP_DEST_DIR"/backup.enc
+  s3cmd get "$AMAZON_SOURCE_DIR"/key.bin.enc $BACKUP_DEST_DIR
+  decrypt_and_uncompress $PRIVATE_KEY $BACKUP_DEST_DIR
 }
